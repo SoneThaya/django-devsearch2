30caa9d79aa263dc0c66d0b84a71626918062e9d
sone <sthaya@gmail.com>
2021-09-18 16:56:45 -0700

    templates

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return HttpResponse('Here are our projects')
+    return render(request, 'projects/projects.html')
 
 def project(request, pk):
-    return HttpResponse('single project ' + str(pk))
+    return render(request, 'projects/single-project.html')
